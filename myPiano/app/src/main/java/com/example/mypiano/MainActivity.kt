95d1b52bf857f1bbd156c7d51b7c83e148ea7eb3
Robin Hansen <robinh18@uia.no>
2021-02-23 13:42:10 +0100

    I had a package problem with the last project, so im making a new one

---
+++ myPiano/app/src/main/java/com/example/mypiano/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mypiano
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ myPiano/app/src/main/java/com/example/mypiano/PianoLayout.kt
@@ -0,0 +1,26 @@
+package com.example.mypiano
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PianoLayout : Fragment() {
+
+    private val fullTones = listOf("C", "D", "E", "F", "G", "A", "B", "C2", "D2", "E2", "F2", "G2", "A2", "B2")
+    private val semiTones = listOf("C#", "D#", "F#", "G#", "A#", "C#2", "D#2", "F#2", "G#2", "A#2")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_piano, container, false)
+    }
+
+}
